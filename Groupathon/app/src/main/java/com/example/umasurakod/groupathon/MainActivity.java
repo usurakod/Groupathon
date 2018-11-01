da23a10be0ed03ae81f4e54ae509a1a4a7378460
Usurakod <umasurakod@gmail.com>
2018-10-31 18:50:54 -0700

    Main activity with lefy navigation added

--- Groupathon/app/src/main/java/com/example/umasurakod/groupathon/MainActivity.java
+++ Groupathon/app/src/main/java/com/example/umasurakod/groupathon/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.umasurakod.groupathon;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout myDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDrawerLayout =(DrawerLayout)findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,myDrawerLayout,R.string.open,R.string.close);
+
+        myDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // to select item from action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
